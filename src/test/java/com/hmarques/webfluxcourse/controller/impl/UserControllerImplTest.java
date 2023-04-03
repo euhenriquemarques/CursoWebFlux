04573b3914c59427d495e7da2b2902e28cd85e0b
euhenriquemarques <hssmarques@gmail.com>
2023-04-03 16:40:30 -0300

    adicionando testes de integração

--- src/test/java/com/hmarques/webfluxcourse/controller/impl/UserControllerImplTest.java
+++ src/test/java/com/hmarques/webfluxcourse/controller/impl/UserControllerImplTest.java
@@ -1,5 +1,8 @@
 package com.hmarques.webfluxcourse.controller.impl;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 import com.hmarques.webfluxcourse.entity.User;
 import com.hmarques.webfluxcourse.mapper.UserMapper;
 import com.hmarques.webfluxcourse.model.request.UserRequest;
@@ -16,6 +19,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
 import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Mono;
 
@@ -41,7 +45,7 @@ class UserControllerImplTest {
   void save() {
     UserRequest userRequest = new UserRequest("henrique", "henrique@henrique.com", "123");
 
-    Mockito.when(service.save(ArgumentMatchers.any(UserRequest.class))).thenReturn(Mono.just(User.builder().build()));
+    Mockito.when(service.save(any(UserRequest.class))).thenReturn(Mono.just(User.builder().build()));
     webTestClient.post()
         .uri("/users")
         .contentType(MediaType.APPLICATION_JSON)
@@ -50,6 +54,28 @@ class UserControllerImplTest {
         .expectStatus()
         .isCreated();
 
+    Mockito.verify(service, Mockito.times(1)).save(any(UserRequest.class));
+
+  }
+
+
+  @Test
+  void saveBadRequest() {
+    UserRequest userRequest = new UserRequest(" henrique", "henrique@henrique.com", "123");
+
+    webTestClient.post()
+        .uri("/users")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(userRequest))
+        .exchange()
+        .expectStatus()
+        .isBadRequest().expectBody()
+        .jsonPath("$.path").isEqualTo("/users")
+        .jsonPath("$.status").isEqualTo(BAD_REQUEST.value())
+        .jsonPath("$.error").isEqualTo("Bad Request")
+        .jsonPath("$.message").isEqualTo("Validation Error on validation atibutes")
+        .jsonPath("$.errors[0].fieldName").isEqualTo("name")
+        .jsonPath("$.errors[0].message").isEqualTo("Não é aceito espaços em brancos!");
   }
 
   @Test
